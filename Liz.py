0d344075c0f8af099b70555f4af41ca315322a32
CristopherNicolas <cristopher.faundez@uniacc.edu>
2024-05-01 18:56:09 -0400

    Lines of code count update

--- Liz.py
+++ Liz.py
@@ -8,11 +8,14 @@ def InitLiz():
         for file in files:
             if file.endswith(".cs"):
                 csFiles.append(os.path.join(root,file))
-    
+    totalLines = 0
     for s in csFiles:
+         f = open(s)
+         lineCount = len( f.readlines())
+         totalLines += lineCount
          className = os.path.basename(s)
-         print(f"Liz -> {className}")
-    print(f"Liz: you have {len(csFiles)} C# scripts") 
+         print(f"Liz -> {className} with {lineCount} lines of code!")
+    print(f"Liz: you have {len(csFiles)} C# scripts with a total of {totalLines} lines of code") 
 
 
 InitLiz()
